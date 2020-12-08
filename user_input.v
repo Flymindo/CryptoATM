961b3e885f61adf0ccbadaf7e798b8766f17e54e
misencan <75406572+misencan@users.noreply.github.com>
2020-12-08 00:27:13 -0500

    Add files via upload

--- user_input.v
+++ user_input.v
@@ -20,19 +20,27 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
+// PROGRAM TO SEND READY SIGNAL FOR EVERYTHING
+
+
 module user_input(
     input clk,
-    input wire[7:0] ascii_code,
-    input wire[10:0] state,
-    input wire input_style_out,
-    input wire status_code,
-    output reg status_codeo,
-    output reg [10:0] next_state,
-    output reg[15:0] pswd,
-    output reg[15:0] acct
+    input  [7:0] ascii_code,
+    input  [3:0] input_style_out,
+    output reg [3:0] status_code_out,
+    output reg [15:0] pswd,
+    output reg [15:0] acct,
+    output reg [1:0] usr_input_out,
+    output reg [2:0] currency_type_out
     );
     
-    // Parameter for status codes for state transitions (status_code)
+    parameter [2:0]
+        USD = 3'b000,
+        BTC = 3'b001,
+        ETH = 3'b010,
+        XRP = 3'b011,
+        LTC = 3'b100;
+        
     parameter [3:0] 
         ACC_FOUND = 4'b0001,
         ACC_NOT_FOUND = 4'b0010,
@@ -43,7 +51,6 @@ module user_input(
         EXIT = 4'b0111,
         INPUT_COMPLETE = 4'b1000;
         
-    // Parameter for Input style (Pretty sure these will work for all state transitions/input requirements, let me know though)
     parameter [3:0]
         SINGLE_KEY = 4'b0001,
         ACC_NUMBER = 4'b0010,
@@ -52,88 +59,145 @@ module user_input(
         CURRENCY_TYPE = 4'b0101,
         CURRENCY_AMOUNT = 4'b0110;
         
-    parameter [4:0] // I made this encoding scheme so i could light up debug LEDs but there are too many states lol
-                     // These will probably change but wont affect any other modules
-        IDLE = 5'b00001,
-        ACC_NUM = 5'b00010,
-        PIN_INPUT = 5'b00011,
-        MENU = 5'b00100,
-        SHOW_BALANCES = 5'b00101,
-        CONVERT_CURRENCY = 5'b00110,
-        SELECT_CURRENCY_CONVERT_1 = 5'b00111,
-        SELECT_CURRENCY_CONVERT_2 = 5'b01000,
-        WITHDRAW = 5'b01001,
-        SELECT_AMOUNT_WITHDRAW = 5'b01010,
-        TRANSFER = 5'b01011,
-        SELECT_CURRENCY_TRANSFER = 5'b01100,
-        SELECT_AMOUNT_TRANSFER = 5'b01101,
-        ERROR = 5'b01110,
-        SUCCESS = 5'b01111;
+    parameter [1:0]
+        BALANCE = 2'b00,
+        CONVERT = 2'b01,
+        WITHDRAW_OPTION = 2'b10,
+        TRANSFER_OPTION = 2'b11;
     
-    parameter[15:0]
-        ACCT1 = 16'b0111011101110111,
-        ACCT2 = 16'b0001000100010001,
-        ACCT3 = 16'b0011001100110011;
-        
-    parameter[15:0]
-        PIN1 = 16'b0111011101110111,
-        PIN2 = 16'b0001000100010001,
-        PIN3 = 16'b0011001100110011;
-        
-    parameter[15:0]
-        BAL1 = 16'b0111011101110111,
-        BAL2 = 16'b0001000100010001,
-        BAL3 = 16'b0011001100110011;
-       
-       
-     reg count =0;
-    // account, pin input
-    always@(posedge clk) begin
-     if(ascii_code == 8'h71) // q   
+    reg count =0;
+    
+    always @(posedge clk) begin
+    
+    case(ascii_code)
+        8'h71: begin   // q
+            status_code_out = EXIT;
+            end
+    endcase
+    
+    
+    case(input_style_out)
+        ACC_NUMBER: begin
+        if (ascii_code !== 8'h2A) begin
+            //Concatenation code for ACC_NUMBER  
+            if(count == 0) begin
+                acct <= ascii_code[3:0]; // 0 is the pin's LSB
+                count <= count+1;
+            end
+            else if(count > 0 && count < 4) begin
+                acct  = {ascii_code[3:0],acct};
+                count <= count+1;
+            end
+            else if(count >= 4) begin
+                count <= 0;
+            end  
+            // ready signal using enter key
+            if(ascii_code == 8'h0D)         // Enter button pressed
                 begin
-                    status_codeo <= EXIT;
-                end   
-        case(state)
-              IDLE:
-                    if(count == 0) begin
-                        acct <= ascii_code[3:0]; // 0 is the pin's LSB
-                        count <= count+1;
-                    end
-                    else if(count > 0 && count < 4) begin
-                        acct  = {ascii_code[3:0],acct};
-                        count <= count+1;
-                    end
-                    else if(count >= 4) begin
-                        count <= 0;
-                    end
-              ACC_NUM:
-                    if(acct == ACCT1 || acct == ACCT2 || acct == ACCT3) begin
-                        status_codeo = ACC_FOUND;
-                    end
-                    else begin
-                        status_codeo = ACC_NOT_FOUND;
-                    end
-              PIN_INPUT: begin
-              if(pswd == PIN1 || pswd == PIN2 || pswd == PIN3) begin
-                    status_codeo = PIN_CORRECT;
-              end
-              else begin
-                status_codeo = PIN_INCORRECT;
-              end
-              if(count == 0) begin
-                        count <= count+1;
-                        pswd <= ascii_code[3:0]; // 0 is the pin's LSB
-                         
+                    status_code_out = INPUT_COMPLETE;
                 end
-                else if(count > 0 && count < 4) begin
-                        pswd  = {ascii_code[3:0], pswd};
-                        count <= count + 1;
-                 end
-                 else if(count >= 4) begin
-                    count <= 0;
-                 end
-                 //assign status_codeo = (pswd == 16'b0000000000000000) ? PIN_INCORRECT : PIN_CORRECT
-                 end
-          endcase
+        end
+        end
+        
+        PIN_NUMBER: begin
+        if (ascii_code !== 8'h2A) begin
+            //Concatenation code for PIN_NUMBER  
+            if(count == 0) begin
+                count <= count+1;
+                pswd <= ascii_code[3:0]; // 0 is the pin's LSB       
+            end
+            else if(count > 0 && count < 4) begin
+                pswd  = {ascii_code[3:0], pswd};
+                count <= count + 1;
+            end
+            else if(count >= 4) begin
+                count <= 0;
+            end
+            if(ascii_code == 8'h0D)         // Enter button pressed
+                begin
+                    status_code_out = INPUT_COMPLETE;
+                end
+        end
+        end
+        
+        MENU_SELECTION: begin
+            case(ascii_code)
+                8'h62: begin                               // b key (balance)
+                    usr_input_out = BALANCE;
+                    end
+                8'h63: begin                               // c (convert currency)
+                    usr_input_out = CONVERT;
+                    end
+                8'h77: begin                               // w (withdraw)
+                    usr_input_out = WITHDRAW_OPTION;
+                    end
+                8'h74: begin                              // t (transfer)
+                    usr_input_out = TRANSFER_OPTION;
+                    end
+            endcase
+            if(ascii_code == 8'h0D)         // Enter button pressed
+                begin
+                    status_code_out = INPUT_COMPLETE;
+                end
+        end
+        
+  
+        SINGLE_KEY: begin                         
+                if(ascii_code == 8'h0D)         // Enter button pressed
+                begin
+                    status_code_out = INPUT_COMPLETE;
+                end               
+        end
+                
+        CURRENCY_TYPE: begin            ///1 to 5 cases         ///  currency_type_out = ascii hex   ///ready signal
+            case(ascii_code)
+                8'h31: begin                            // 1 - USD
+                    currency_type_out = USD;
+                    end
+                8'h32: begin                            // 2 - BTC
+                    currency_type_out = BTC;
+                    end
+                8'h33: begin                           // 3 - ETH
+                    currency_type_out = ETH;
+                    end
+                8'h34: begin                           // 4 - XRP
+                    currency_type_out = XRP;
+                    end
+                8'h35: begin                           // 5 - LTC
+                    currency_type_out = LTC;
+                    end
+            endcase
+            if(ascii_code == 8'h0D)         // Enter button pressed
+                begin
+                    status_code_out = INPUT_COMPLETE;
+                end
+        end
+            
+            
+        CURRENCY_AMOUNT: begin       
+        
+              //// until max out 8 digits or they hit enter /////////////////////////////////
+              
+            if(ascii_code == 8'h0D)         // Enter button pressed
+                begin
+                    status_code_out = INPUT_COMPLETE;
+                end
+        end
+             
+             
+             
+
+    endcase
     end
+    
+    
 endmodule
+
+
+
+
+
+
+
+
+
